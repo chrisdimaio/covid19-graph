4a71d1b825e6bdfc7298d0ca519322366356a020
Chris DiMaio <chrisdimaio@gmail.com>
2020-03-29 19:09:47 -0400

    Changed file name and add comment

--- data_processor/build_zip.sh
+++ data_processor/build_zip.sh
@@ -1,11 +1,13 @@
 #! /usr/bin/sh
 
+# A temporary way to build Lambda zip without exceeding 10mb.
+
 cd ../venv/lib/python3.7/site-packages/
 
-zip -r9 ${OLDPWD}/covid19_graph2.zip requests*
-zip -r9 ${OLDPWD}/covid19_graph2.zip chardet*
-zip -r9 ${OLDPWD}/covid19_graph2.zip certifi*
-zip -r9 ${OLDPWD}/covid19_graph2.zip idna*
+zip -r9 ${OLDPWD}/covid19_graph.zip requests*
+zip -r9 ${OLDPWD}/covid19_graph.zip chardet*
+zip -r9 ${OLDPWD}/covid19_graph.zip certifi*
+zip -r9 ${OLDPWD}/covid19_graph.zip idna*
 
 cd $OLDPWD
-zip -g covid19_graph2.zip lambda_function.py
+zip -g covid19_graph.zip lambda_function.py
